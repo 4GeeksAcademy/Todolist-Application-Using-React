427c5c33fcb2b0b0b3a5beead20c5bc787580ef5
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-06-01 19:01:10 +0000

    ejercicio sin acabar

--- src/js/component/todoList.js
+++ src/js/component/todoList.js
@@ -1,36 +1,40 @@
 
+import { number } from "prop-types";
 import React, { useState, useEffect } from "react";
 
 export default function TodoList() {
 
-    const [input, setInput] = useState('Añadir tarea');
+    const [input, setInput] = useState('');
     const [todoList, setTodoList] = useState([]);
+    const [id, setId] = useState(1);
 
     const handleKeyDown = (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
-            if (input.trim() !== "") {
-                setTodoList((preTodoList) => [...preTodoList, input.trim()]);
-                setInput("Qué tarea quieres hacer?");
-            }
+
+            const newTask = {
+                id: id,
+                task: input
+            };
+
+            setTodoList((prevTodoList) => [...prevTodoList, newTask]);
+            setId((preId) => preId + 1);
+            setInput("");
         }
     };
-  
-    const handleItemClick = (id) => {
 
-         
-    };
+
     return (
         <>
-            <form class="row">
+            <form className="row">
                 <div className="col-md-8">
-                    <input type="text" name="todoList" className="form-control" onKeyDown={handleKeyDown} onChange={e => setInput(e.target.value)} value={input} id="validationServer01" />
+                    <input type="text" name="todoList" placeholder="Añadir tarea" className="form-control" onKeyDown={handleKeyDown} onChange={e => setInput(e.target.value)} value={input} id="validationServer01" />
                     <ul>
-                        {todoList.map((element, index) => (
-                              <li key={element.id} onClick={() => handleItemClick(element.id)}>
-                              {element.task}
-                              </li>
-                        ))}
+                        {todoList.map((element, index) => {
+                            <li key={element.id}>
+                                {element.task}
+                            </li>
+                        })}
                     </ul>
                 </div>
             </form>
